5dbf4bc74a3b0184a9608f3f618958488cb96651
Faiz Faidurrahman <faizfaidurrahman@gmail.com>
2023-04-12 14:55:02 +0300

    wip

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
+
+const isElectron = !!process.env.NEXT_PUBLIC_IS_ELECTRON;
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer && isElectron) {
+      config.target = 'electron-renderer';
+    }
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
