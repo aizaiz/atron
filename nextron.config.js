4f64cbc7300a6a5330bb8cf6de1a672ea002a4de
Faiz Faidurrahman <faizfaidurrahman@gmail.com>
2023-04-12 17:17:54 +0300

    fix electron dev and build

--- nextron.config.js
+++ nextron.config.js
@@ -3,10 +3,15 @@ module.exports = {
   mainSrcDir: 'main',
   // specify an alternate renderer src directory, defaults to 'renderer'
   rendererSrcDir: '.',
-
+  // main: {
+  //   output: 'path/to/your/output/crazy', // Change this to your desired folder
+  // },
+  // renderer: {
+  //   output: 'path/to/your/output/crazy/renderer', // Change this to your desired folder for the renderer
+  // },
   // main process' webpack config
   webpack: (config, env) => {
     // do some stuff here
     return config;
   },
-};
+};  
